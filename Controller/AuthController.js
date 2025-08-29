349b384eb0b7eb4eb761627743da80e39f2314b2
VELAGA SUBHASH <130762017+VELAGASUBHASH@users.noreply.github.com>
2025-08-29 10:22:34 +0530

    Update AuthController.js

--- Controller/AuthController.js
+++ Controller/AuthController.js
@@ -25,20 +25,22 @@ const storeRefreshToken = async(userId,refreshtoken)=>{
     await redis.set(`refresh_token:${userId}`,refreshtoken,"EX",7*24*60*60);
 }
 
-const setCookie = (res,accestoken,refreshtoken) =>{
-    res.cookie("accessToken",accestoken,{
-        httpOnly: true,
-        secure:process.env.NODE_ENV === "production",
-        sameSite: "strict",
-        maxAge: 15*60*1000,
-    });
-        res.cookie("refreshToken",refreshtoken,{
-        httpOnly: true,
-        secure:process.env.NODE_ENV === "production",
-        sameSite: "strict",
-        maxAge: 7*24*60*60*1000,
-    });
+const setCookie = (res, accestoken, refreshtoken) => {
+  res.cookie("accessToken", accestoken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "none",
+    maxAge: 15 * 60 * 1000, // 15 minutes
+  });
+
+  res.cookie("refreshToken", refreshtoken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "none",
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  });
 };
+
 
 
 export const signup = async (req, res) => {
